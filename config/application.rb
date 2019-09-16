62318748c7a4f2885f49e963642c068f8c33fbe7
morispring <morikazunori0414@gmail.com>
2019-09-16 17:36:08 +0900

    slim gem

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module MyApp
       g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
 
+    config.generators.template_engine = :slim
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
